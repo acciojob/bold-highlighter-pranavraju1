c729e7d90882bb5f81d702cad89aa7302719db8b
Pranav Raju <120582474+pranavraju1@users.noreply.github.com>
2023-10-08 14:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let bold = document.getElementsByTagName("strong");
+console.log(bold);
+
 function highlight() {
     //Write your code here
-
-
+    for (let i = 0; i < bold.length; i++) {
+        bold[i].style.color="green";
+    }
 }
+
 
 
 function return_normal() {
     //Write your code here
-
+	    for (let i = 0; i < bold.length; i++) {
+        bold[i].style.color="black";
+    }
     
 }
